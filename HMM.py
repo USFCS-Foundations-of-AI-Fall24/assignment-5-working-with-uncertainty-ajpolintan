8215a64c237d5f5235db1036c787c1de7909c2d5
ajpolintan <85649481+ajpolintan@users.noreply.github.com>
2024-10-29 15:29:12 -0700

    Finished load

--- HMM.py
+++ HMM.py
@@ -38,7 +38,46 @@ class HMM:
         """reads HMM structure from transition (basename.trans),
         and emission (basename.emit) files,
         as well as the probabilities."""
-        pass
+        print(basename)
+        with open(str(basename) + ".emit") as f:
+            for lines in f : 
+                words = lines.rstrip('\n').split(" ")
+                                #start of edge
+                key = words[0]
+                #end of edge
+                edge = words[1]
+                #value of the edge
+                value = words[2]
+
+                #https://www.geeksforgeeks.org/python-check-given-key-already-exists-in-a-dictionary/ referenced this 
+                #check if key exists
+                if key not in self.emissions :
+                    self.emissions[key] = {} 
+                    self.emissions[key][edge] = value
+                else :
+                    self.emissions[key][edge] = value
+
+                print(len(self.emissions[words[0]]))
+
+        with open(str(basename) + ".trans") as f:
+            for lines in f : 
+                words = lines.rstrip('\n').split(" ")
+                                #start of edge
+                key = words[0]
+                #end of edge
+                edge = words[1]
+                #value of the edge
+                value = words[2]
+
+                #https://www.geeksforgeeks.org/python-check-given-key-already-exists-in-a-dictionary/ referenced this 
+                #check if key exists
+                if key not in self.transitions :
+                    self.transitions[key] = {} 
+                    self.transitions[key][edge] = value
+                else :
+                    self.transitions[key][edge] = value
+
+        return 
 
 
    ## you do this.
@@ -61,7 +100,13 @@ class HMM:
     ## You do this. Given a sequence with a list of emissions, fill in the most likely
     ## hidden states using the Viterbi algorithm.
 
-
+if __name__ == "__main__":
+    #print(car_infer.query(variables=["Moves"],evidence={"Radio":"turns on", "Starts":"yes"}))
+    h = HMM()
+    h.load('cat')
+    print(h.transitions)
+    print("------------------------")
+    print(h.emissions)
 
 
 
